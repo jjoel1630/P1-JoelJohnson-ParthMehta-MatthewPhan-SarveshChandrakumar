3f654b6fbcd36857d74ee64241a428bc9907f1f7
Joel Johnson <jjoel1630@gmail.com>
2022-12-08 09:35:54 -0800

    added apiutil file for easy functions to api calls. easier to read

---
+++ apiutil.php
@@ -0,0 +1,25 @@
+<?php 
+	// $request = getReq("https://www.omdbapi.com/?apikey=" . $_ENV["OMBD_API_KEY"] . "&t=Fury");
+	// $request = getReq("https://api.themoviedb.org/3/movie/436270?api_key=" . $_ENV["THE_MOVIEDB_KEY"]);
+	// $request = json_decode($request);
+
+	// echo getReq("https://www.omdbapi.com/?apikey=db60c827&t=Fury");
+	// echo $request->Title;
+
+	// GET REQUEST API
+	function getReq($url)
+	{
+		$curl = curl_init($url);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, [
+			'Content-Type: application/json'
+		]);
+
+		$result = curl_exec($curl);
+
+		curl_close($curl);
+
+		return $result;
+	}
+?>
