03137cbf127646f3ffe22b600622084daa0bd9d0
Joel Johnson <jjoel1630@gmail.com>
2022-12-08 10:02:28 -0800

    coded basic navbar

--- index.php
+++ index.php
@@ -7,5 +7,35 @@
 	include 'apiutil.php';
 	include 'dbutil.php';
 
-	getData();
+	// getData();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+	<link rel="stylesheet" href="css/navbar.css" type="text/css">
+</head>
+<body>
+	<!-- NAVBAR -->
+		<!-- LIST OF MOVIES, HOME, SEARCH (later) -->
+		<nav>
+			<div>
+				<h3>TITLE OF WEBSITE</h3>
+			</div>
+			<ul>
+				<li>List</li>
+				<li>Movies</li>
+				<li>Reviews</li>
+			</ul>
+		</nav>
+	<!-- HOME BANNER (TITLE, ETC) -->
+	<!-- INFO ABOUT US -->
+	<!-- WHAT THE WEBSITE IS FOR -->
+	<!-- HOW TO USE -->
+	<!-- FOOTER -->
+</body>
+</html>
