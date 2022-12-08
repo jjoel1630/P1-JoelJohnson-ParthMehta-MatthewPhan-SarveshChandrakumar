fcc688f8dd6f9dc033d4ccf12fed98a9e3d0f6fc
Joel Johnson <jjoel1630@gmail.com>
2022-12-07 18:24:00 -0800

    set up mysql, composer, .env

---
+++ test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<?php
+		require 'vendor/autoload.php';
+
+		$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+		$dotenv->load();
+		// var_dump($_ENV);
+
+		$servername = "localhost";
+		$username = "root";
+		$password = $_ENV["SQL_PWD"];
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		echo "Connected successfully";
+	?>
+</body>
+</html>
