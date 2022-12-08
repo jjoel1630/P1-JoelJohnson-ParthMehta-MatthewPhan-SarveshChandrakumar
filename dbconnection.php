b43c0529ffcaed6bb1b0a94a85705fbd8d4a1f97
Joel Johnson <jjoel1630@gmail.com>
2022-12-08 09:36:29 -0800

    added a file for connection to db. other files call that function to connect to database. returnes connection object

---
+++ dbconnection.php
@@ -0,0 +1,21 @@
+<?php
+	require 'vendor/autoload.php';
+
+	function connectMoviesDB() {
+		// var_dump($_ENV);
+
+		// DB CONFIG
+		$servername = "localhost";
+		$username = "javellecousins";
+		$password = $_ENV["JAVELLECOUSINS"];
+	
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, "csp_project_database");
+	
+		// Check connection
+		if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+		// else echo "Success";
+
+		return $conn;
+	}
+?>
